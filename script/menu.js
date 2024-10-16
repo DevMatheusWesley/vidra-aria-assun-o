d47e1ad74750ec579d0dc3b834feec01fc17efae
Matheus Wesley <131497001+DevMatheusWesley@users.noreply.github.com>
2024-10-16 16:27:55 -0300

    Add files via upload

--- script/menu.js
+++ script/menu.js
@@ -7,3 +7,11 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
